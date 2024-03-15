1de195ca6b36c2e79ed602faa266bf8eb67b4fbe
Luis270401 <carrizozajoseluis@gmail.com>
2024-03-15 12:01:23 -0600

    Modelado de la BD

---
+++ SorteoEstacionamiento/Server/CapaDataAccess/DBContext/Tbganadore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SorteoEstacionamiento.Server.CapaDataAccess.DBContext;
+
+public partial class Tbganadore
+{
+    public int IdGanador { get; set; }
+
+    public int GanIdParticipante { get; set; }
+
+    public string GanSorteoAm { get; set; } = null!;
+
+    public virtual Tbparticipante GanIdParticipanteNavigation { get; set; } = null!;
+}
