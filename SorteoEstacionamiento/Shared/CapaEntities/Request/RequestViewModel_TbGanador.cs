2c4d40fd899c55aee3fb6e771a59359c39569045
VictorGAM22 <avictorgibran@yahoo.com>
2024-03-22 13:52:07 -0600

    tbganador

--- SorteoEstacionamiento/Shared/CapaEntities/Request/RequestViewModel_TbGanador.cs
+++ SorteoEstacionamiento/Shared/CapaEntities/Request/RequestViewModel_TbGanador.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SorteoEstacionamiento.Shared.CapaEntities.Request
 {
-    internal class RequestViewModel_TbGanador
+    public class RequestViewModel_TbGanador
     {
+        [Key]
+        public int IdGanador { get; set; }
+        //Atributos de la tabla TbGanador
+        [Column("ganIdParticipante")]
+        [StringLength(11)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Campo requerido.")]
+        public string? ganIdParticipante { get; set; }
+        //*******************************************************************************
+        [Column("ganSorteoAM")]
+        [StringLength(1)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Campo requerido.")]
+        public string? ganSorteoAM { get; set; }
     }
 }
