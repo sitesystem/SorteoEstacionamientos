b78d094fe42d4e318e3f86fb8afd60cbdbef14df
Luis270401 <carrizozajoseluis@gmail.com>
2024-03-22 11:45:43 -0600

    Controlador del tipoparticipante

--- SorteoEstacionamiento/Server/CapaDataAccess/Controllers/TipoParticipantesController.cs
+++ SorteoEstacionamiento/Server/CapaDataAccess/Controllers/TipoParticipantesController.cs
@@ -1,83 +1,92 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SorteoEstacionamiento.Server.CapaDataAccess.DBContext;
+
+using SorteoEstacionamiento.Shared.CapaEntities.Request;
+using SorteoEstacionamiento.Shared.CapaEntities.Response;
+
 
 namespace SorteoEstacionamiento.Server.CapaDataAccess.Controllers
 {
-    public class TipoParticipantesController : Controller
+    public class TipoParticipantesController(SorteoestacionamientosContext db) : ControllerBase
     {
-        // GET: TipoParticipantesController
-        public ActionResult Index()
-        {
-            return View();
-        }
+        private readonly SorteoestacionamientosContext _db = db;
 
-        // GET: TipoParticipantesController/Details/5
-        public ActionResult Details(int id)
+        //Metodo para obtener la informacion mediante el id
+        [HttpGet("filterById/{id}")]
+        public async Task<IActionResult> GetDataById(int id)
         {
-            return View();
-        }
+            Response<Cattipoparticipante> oResponse = new();
 
-        // GET: TipoParticipantesController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: TipoParticipantesController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var item = await _db.Cattipoparticipantes.FindAsync(id);
+                oResponse.Success = 1;
+                oResponse.Data = item;
             }
-            catch
-            {
-                return View();
+            catch (Exception ex) 
+            { 
+                oResponse.Message = ex.Message;
             }
+
+            return Ok(oResponse);
         }
 
-        // GET: TipoParticipantesController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: TipoParticipantesController/Edit/5
+        //Metodo para agregar valor
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> AddData(RequestViewModel_TipoParticipante model)
         {
+            Response<object> oResponse = new();
+
             try
             {
-                return RedirectToAction(nameof(Index));
+                Cattipoparticipante oTipoParticipante = new()
+                {
+                    IdTipoParticipante = model.IdTipoParticipante,
+                    TipoParticipante = model.TipoParticipante
+                };
+
+                await _db.Cattipoparticipantes.AddAsync(oTipoParticipante);
+                await _db.SaveChangesAsync();
+
+                oResponse.Success = 1;
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                oResponse.Message = ex.Message; 
             }
+
+            return Ok(oResponse);
         }
 
-        // GET: TipoParticipantesController/Delete/5
-        public ActionResult Delete(int id)
+        //Metodo para editar valores
+        [HttpPut]
+        public async Task<IActionResult> EditData(RequestViewModel_TipoParticipante model)
         {
-            return View();
-        }
+            Response<object> oResponse = new();
 
-        // POST: TipoParticipantesController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
             try
             {
-                return RedirectToAction(nameof(Index));
+                Cattipoparticipante? oTipoParticipante = await _db.Cattipoparticipantes.FindAsync(model.IdTipoParticipante);
+
+                if (oTipoParticipante != null)
+                {
+                    oTipoParticipante.TipoParticipante = model.TipoParticipante;
+
+                    _db.Entry(oTipoParticipante).State = EntityState.Modified;
+                    await _db.SaveChangesAsync();
+                }
+
+                oResponse.Success = 1;
             }
-            catch
+            catch(Exception ex)
             {
-                return View();
+                oResponse.Message = ex.Message; 
             }
+
+            Ok(oResponse);
         }
+
     }
 }
---
+++ SorteoEstacionamiento/Shared/CapaEntities/Request/RequestViewModel_TipoParticipante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SorteoEstacionamiento.Shared.CapaEntities.Request
+{
+    public class RequestViewModel_TipoParticipante
+    {
+        [Key]
+        public int IdTipoParticipante { get; set; }
+
+        //Segundo campo nuestra tabla del catalogo TipoParticipante, y se refiere al nombre del tipo
+        [Column("TipoParticipante")]
+        [StringLength(50)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Campo requerido.")]
+        public string? TipoParticipante { get; set; }
+
+    }
+}
