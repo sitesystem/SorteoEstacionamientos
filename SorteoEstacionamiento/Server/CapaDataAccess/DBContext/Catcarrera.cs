f0556f0f7c6c408b2a639c0246e2eaf4a5088433
Luis270401 <carrizozajoseluis@gmail.com>
2024-03-22 13:25:14 -0600

    a

--- SorteoEstacionamiento/Server/CapaDataAccess/DBContext/Catcarrera.cs
+++ SorteoEstacionamiento/Server/CapaDataAccess/DBContext/Catcarrera.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
 
 namespace SorteoEstacionamiento.Server.CapaDataAccess.DBContext;
 
+[Table("catCarreras")]
 public partial class Catcarrera
 {
+    [Key]
     public int IdCarrera { get; set; }
 
-    public string Carrera { get; set; } = null!;
+    [Column("Carrera")]
+    [StringLength(50)]
+    public string? Carrera { get; set; } = null!;
 
+    [JsonIgnore]
+    [InverseProperty("PartIdCarreraNavigation")]
     public virtual ICollection<Tbparticipante> Tbparticipantes { get; set; } = new List<Tbparticipante>();
 }
--- SorteoEstacionamiento/Server/CapaDataAccess/DBContext/Cattipoparticipante.cs
+++ SorteoEstacionamiento/Server/CapaDataAccess/DBContext/Cattipoparticipante.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
 
 namespace SorteoEstacionamiento.Server.CapaDataAccess.DBContext;
 
+[Table("catTipoParticipante")]
 public partial class Cattipoparticipante
 {
+    [Key]
     public int IdTipoParticipante { get; set; }
 
+    [Column("TipoParticipante")]
+    [StringLength(50)]
     public string TipoParticipante { get; set; } = null!;
 
+    [JsonIgnore]
+    [InverseProperty("PartIdTipoParticipanteNavigation")]
     public virtual ICollection<Tbparticipante> Tbparticipantes { get; set; } = new List<Tbparticipante>();
 }
