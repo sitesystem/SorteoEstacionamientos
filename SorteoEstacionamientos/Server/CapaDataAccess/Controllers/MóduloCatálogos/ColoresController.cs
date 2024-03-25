9c701b7789e485e45f3e878654d550b9a84a7407
VictorGAM22 <avictorgibran@yahoo.com>
2024-03-25 13:00:28 -0600

    API COLORES

--- SorteoEstacionamientos/Server/CapaDataAccess/Controllers/MóduloCatálogos/ColoresController.cs
+++ SorteoEstacionamientos/Server/CapaDataAccess/Controllers/MóduloCatálogos/ColoresController.cs
@@ -39,5 +39,106 @@ namespace SorteoEstacionamientos.Server.CapaDataAccess.Controllers.MóduloCatál
             }
             return Ok(oResponse);
         }
+
+        [HttpGet("filterById/{id}")]
+        public async Task<IActionResult> GetDataById(int id)
+        {
+            Response<McCatColore> oResponse = new();
+
+            try
+            {
+                var item = await _db.McCatColores.FindAsync(id);
+                oResponse.Success = 1;
+                oResponse.Data = item;
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+
+            return Ok(oResponse);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddData(RequestViewModel_Color model)
+        {
+            Response<object> oResponse = new();
+
+            try
+            {
+                McCatColore oColor = new()
+                {
+                    IdColor = model.IdColor,
+                    ColorNombre = model.ColorNombre
+                };
+
+                await _db.McCatColores.AddAsync(oColor);
+                await _db.SaveChangesAsync();
+
+                oResponse.Success = 1;
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+
+            return Ok(oResponse);
+        }
+        [HttpPut]
+        public async Task<IActionResult> EditData(RequestViewModel_Color model)
+        {
+            Response<object> oRespuesta = new();
+
+            try
+            {
+                McCatColore? oColor = await _db.McCatColores.FindAsync(model.IdColor);
+
+                if (oColor != null)
+                {
+                    oColor.IdColor = model.IdColor;
+                    oColor.ColorNombre = model.ColorNombre;
+
+                    _db.Entry(oColor).State = EntityState.Modified;
+                    await _db.SaveChangesAsync();
+                }
+
+                oRespuesta.Success = 1;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Message = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+
+        [HttpPut("editByIdStatus/{id}/{isActivate}")]
+        public async Task<IActionResult> EnableDisableDataById(int id, sbyte isActivate)
+        {
+            Response<object> oRespuesta = new();
+
+            try
+            {
+                McCatColore? oColor = await _db.McCatColores.FindAsync(id);
+
+                //db.Remove(oPersona);
+                if (oColor != null)
+                {
+                    oColor.IdColor = isActivate;
+                    _db.Entry(oColor).State = EntityState.Modified;
+                    await _db.SaveChangesAsync();
+                }
+
+                oRespuesta.Success = 1;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Message = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
     }
 }
