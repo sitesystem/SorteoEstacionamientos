166677c31cd8b3b8fdc44c82613882b69a166944
VictorGAM22 <avictorgibran@yahoo.com>
2024-03-25 13:59:35 -0600

    ApiSemestre

---
+++ SorteoEstacionamientos/Server/CapaDataAccess/Controllers/MóduloCatálogos/SemestresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SorteoEstacionamientos.Server.CapaDataAccess.Controllers.MóduloCatálogos
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SemestresController : ControllerBase
+    {
+
+    }
+}
