67a1f5ca959bf8650fa35e2eea8cd0638dcc98ac
VictorGAM22 <avictorgibran@yahoo.com>
2024-03-26 13:00:10 -0600

    APIS PERIODOREGISTRO, TIPOVEHICULO

---
+++ SorteoEstacionamientos/Server/CapaDataAccess/Controllers/MóduloCatálogos/PeriodoRegistrosController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SorteoEstacionamientos.Server.CapaDataAccess.DBContext;
+using SorteoEstacionamientos.Shared.CapaEntities.Request;
+using SorteoEstacionamientos.Shared.CapaEntities.Response;
+
+namespace SorteoEstacionamientos.Server.CapaDataAccess.Controllers.MóduloCatálogos
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeriodoRegistrosController(DBSorteoParkingContext db) : ControllerBase
+    {
+        private readonly DBSorteoParkingContext _db = db;
+        [HttpGet("filterByStatus/{filterByStatus}")]
+        public async Task<IActionResult> GetAllDataByStatus(short filterByStatus)
+        {
+            Response<List<MsCatPeriodoRegistro>> oResponse = new();
+            try
+            {
+                var list = new List<MsCatPeriodoRegistro>();
+
+                if (filterByStatus == 1)
+                    list = await _db.MsCatPeriodoRegistros.Where(c => c.IdPeriodoRegistro == filterByStatus).ToListAsync();
+                else
+                    list = await _db.MsCatPeriodoRegistros.ToListAsync();
+
+                if (list == null)
+                    return BadRequest(oResponse);
+
+                oResponse.Success = 1;
+                oResponse.Data = list;
+
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+
+            }
+            return Ok(oResponse);
+        }
+
+
+        [HttpGet("filterById/{id}")]
+        public async Task<IActionResult> GetDataById(int id)
+        {
+            Response<MsCatPeriodoRegistro> oResponse = new();
+
+            try
+            {
+                var item = await _db.MsCatPeriodoRegistros.FindAsync(id);
+                oResponse.Success = 1;
+                oResponse.Data = item;
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+
+            return Ok(oResponse);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddData(RequestDTO_PeriodoRegistro model)
+        {
+            Response<object> oResponse = new();
+
+            try
+            {
+                MsCatPeriodoRegistro oPeriodoRegistro = new()
+                {
+                    IdPeriodoRegistro = model.IdPeriodoRegistro,
+                    PerFechaHoraInicio = model.PerFechaHoraInicio,
+                    PerFechaHoraFin = model.PerFechaHoraFin,
+                    PerFileNameConvocatoria = model.PerFileNameConvocatoria,
+                    PerStatus = model.PerStatus
+                };
+
+                await _db.MsCatPeriodoRegistros.AddAsync(oPeriodoRegistro);
+                await _db.SaveChangesAsync();
+
+                oResponse.Success = 1;
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+
+            return Ok(oResponse);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> EditData(RequestDTO_PeriodoRegistro model)
+        {
+            Response<object> oRespuesta = new();
+
+            try
+            {
+                MsCatPeriodoRegistro? oPeriodoRegistro = await _db.MsCatPeriodoRegistros.FindAsync(model.IdPeriodoRegistro);
+
+                if (oPeriodoRegistro != null)
+                {
+                    oPeriodoRegistro.IdPeriodoRegistro = model.IdPeriodoRegistro;
+                    oPeriodoRegistro.PerFechaHoraInicio = model.PerFechaHoraInicio;
+                    oPeriodoRegistro.PerFechaHoraFin = model.PerFechaHoraFin;
+                    oPeriodoRegistro.PerFileNameConvocatoria = model.PerFileNameConvocatoria;
+                    oPeriodoRegistro.PerStatus = model.PerStatus;
+
+
+
+                    _db.Entry(oPeriodoRegistro).State = EntityState.Modified;
+                    await _db.SaveChangesAsync();
+                }
+
+                oRespuesta.Success = 1;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Message = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut("editByIdStatus/{id}/{isActivate}")]
+        public async Task<IActionResult> EnableDisableDataById(int id, sbyte isActivate)
+        {
+            Response<object> oRespuesta = new();
+
+            try
+            {
+                MsCatPeriodoRegistro? oPeriodoRegistro = await _db.MsCatPeriodoRegistros.FindAsync(id);
+
+                //db.Remove(oPersona);
+                if (oPeriodoRegistro != null)
+                {
+                    oPeriodoRegistro.PerStatus = isActivate;
+                    _db.Entry(oPeriodoRegistro).State = EntityState.Modified;
+                    await _db.SaveChangesAsync();
+                }
+
+                oRespuesta.Success = 1;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Message = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+    }
+}
---
+++ SorteoEstacionamientos/Server/CapaDataAccess/Controllers/MóduloCatálogos/TipoVehiculosController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SorteoEstacionamientos.Server.CapaDataAccess.DBContext;
+using SorteoEstacionamientos.Shared.CapaEntities.Request;
+using SorteoEstacionamientos.Shared.CapaEntities.Response;
+
+namespace SorteoEstacionamientos.Server.CapaDataAccess.Controllers.MóduloCatálogos
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoVehiculosController(DBSorteoParkingContext db) : ControllerBase
+    {
+        private readonly DBSorteoParkingContext _db = db;
+        [HttpGet("filterByStatus/{filterByStatus}")]
+        public async Task<IActionResult> GetAllDataByStatus(short filterByStatus)
+        {
+            Response<List<McCatTipoVehiculo>> oResponse = new();
+            try
+            {
+                var list = new List<McCatTipoVehiculo>();
+
+                if (filterByStatus == 1)
+                    list = await _db.McCatTipoVehiculos.Where(c => c.IdTipoVehiculo == filterByStatus).ToListAsync();
+                else
+                    list = await _db.McCatTipoVehiculos.ToListAsync();
+
+                if (list == null)
+                    return BadRequest(oResponse);
+
+                oResponse.Success = 1;
+                oResponse.Data = list;
+
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+
+            }
+            return Ok(oResponse);
+        }
+
+
+        [HttpGet("filterById/{id}")]
+        public async Task<IActionResult> GetDataById(int id)
+        {
+            Response<McCatTipoVehiculo> oResponse = new();
+
+            try
+            {
+                var item = await _db.McCatTipoVehiculos.FindAsync(id);
+                oResponse.Success = 1;
+                oResponse.Data = item;
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+
+            return Ok(oResponse);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddData(RequestViewModel_TipoVehiculo model)
+        {
+            Response<object> oResponse = new();
+
+            try
+            {
+                McCatTipoVehiculo oTipoVehiculo = new()
+                {
+                    IdTipoVehiculo = model.IdTipoVehiculo,
+                    TipoVehiculo = model.TipoVehiculo
+                };
+
+                await _db.McCatTipoVehiculos.AddAsync(oTipoVehiculo);
+                await _db.SaveChangesAsync();
+
+                oResponse.Success = 1;
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+
+            return Ok(oResponse);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> EditData(RequestViewModel_TipoVehiculo model)
+        {
+            Response<object> oRespuesta = new();
+
+            try
+            {
+                McCatTipoVehiculo? oTipoVehiculo = await _db.McCatTipoVehiculos.FindAsync(model.IdTipoVehiculo);
+
+                if (oTipoVehiculo != null)
+                {
+                    oTipoVehiculo.IdTipoVehiculo = model.IdTipoVehiculo;
+                    oTipoVehiculo.TipoVehiculo = model.TipoVehiculo;
+                   
+
+                    _db.Entry(oTipoVehiculo).State = EntityState.Modified;
+                    await _db.SaveChangesAsync();
+                }
+
+                oRespuesta.Success = 1;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Message = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut("editByIdStatus/{id}/{isActivate}")]
+        public async Task<IActionResult> EnableDisableDataById(int id, sbyte isActivate)
+        {
+            Response<object> oRespuesta = new();
+
+            try
+            {
+                McCatTipoVehiculo? oTipoVehiculo = await _db.McCatTipoVehiculos.FindAsync(id);
+
+                //db.Remove(oPersona);
+                if (oTipoVehiculo != null)
+                {
+                    oTipoVehiculo.IdTipoVehiculo = isActivate;
+                    _db.Entry(oTipoVehiculo).State = EntityState.Modified;
+                    await _db.SaveChangesAsync();
+                }
+
+                oRespuesta.Success = 1;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Message = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+
+
+
+    }
+}
