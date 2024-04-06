b171c8e49b8699b2a5d6614f0dd116c29428e845
Luis270401 <carrizozajoseluis@gmail.com>
2024-04-06 17:30:22 -0600

    y

---
+++ SorteoEstacionamiento/Server/CapaDataAccess/DBContext/Cattipoplaca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SorteoEstacionamiento.Server.CapaDataAccess.DBContext;
+
+public partial class Cattipoplaca
+{
+    public int IdTipoPlaca { get; set; }
+
+    public string TipoPlaca { get; set; } = null!;
+
+    public virtual ICollection<Tbparticipante> Tbparticipantes { get; set; } = new List<Tbparticipante>();
+}
