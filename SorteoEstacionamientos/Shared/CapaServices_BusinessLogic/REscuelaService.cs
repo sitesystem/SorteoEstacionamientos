1814f5c77cffccdecb3a1fe5de839cc5f6d5133b
EduardoCarrasco3 <ecarrascocastillo3@gmail.com>
2024-04-10 13:24:30 -0600

    Creacion de Servicios
    Servicios de carrera, Color, EdoRepMex, Escuela, Link

---
+++ SorteoEstacionamientos/Shared/CapaServices_BusinessLogic/REscuelaService.cs
@@ -0,0 +1,62 @@
+﻿using SorteoEstacionamientos.Shared.CapaEntities.Request;
+using SorteoEstacionamientos.Shared.CapaEntities.Response;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SorteoEstacionamientos.Shared.CapaServices_BusinessLogic
+{
+    public class REscuelaService(HttpClient httpClient) : IGenericService<RequestViewModel_Escuela>
+    {
+        private readonly HttpClient _httpClient = httpClient;
+        private readonly JsonSerializerOptions _options = new() { PropertyNameCaseInsensitive = true };
+        const string url = "/api/Escuelas";
+
+        public async Task<Response<List<RequestViewModel_Escuela>>?> GetAllDataByStatusAsync(bool filterByStatus)
+        {
+            var response = await _httpClient.GetAsync($"{url}/filterByStatus/{filterByStatus}");
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<Response<List<RequestViewModel_Escuela>>>(content, options: _options);
+            return result;
+        }
+
+        public async Task<Response<RequestViewModel_Escuela>?> GetDataByIdAsync(int? id)
+        {
+            var result = await _httpClient.GetFromJsonAsync<Response<RequestViewModel_Escuela>>($"{url}/filterById/{id}", options: _options);
+            return result;
+        }
+
+        public async Task<HttpResponseMessage> AddDataAsync(RequestViewModel_Escuela oEscuela)
+        {
+            //var json = JsonSerializer.Serialize(oCarrera);
+            //var content = new StringContent(json, Encoding.UTF8, "application/json");
+            //var response = await _httpClient.PostAsync(url, content);
+            var response = await _httpClient.PostAsJsonAsync(url, oEscuela, options: _options);
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> EditDataAsync(RequestViewModel_Escuela oColor)
+        {
+            //var json = JsonSerializer.Serialize(oCarrera);
+            //var content = new StringContent(json, Encoding.UTF8, "application/json");
+            //var response = await _httpClient.PutAsync(url, content);
+            var response = await _httpClient.PutAsJsonAsync(url, oColor, options: _options);
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> EnableDisableDataByIdAsync(int id, bool isActivate)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"{url}/editByIdStatus/{id}/{isActivate}",
+                new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+            return response;
+        }
+    }
+}
---
+++ SorteoEstacionamientos/Shared/CapaServices_BusinessLogic/RLinkService.cs
@@ -0,0 +1,62 @@
+﻿using SorteoEstacionamientos.Shared.CapaEntities.Request;
+using SorteoEstacionamientos.Shared.CapaEntities.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SorteoEstacionamientos.Shared.CapaServices_BusinessLogic
+{
+    public class RLinkService(HttpClient httpClient) : IGenericService<RequestViewModel_Link>
+    {
+        private readonly HttpClient _httpClient = httpClient;
+        private readonly JsonSerializerOptions _options = new() { PropertyNameCaseInsensitive = true };
+        const string url = "/api/Links";
+
+        public async Task<Response<List<RequestViewModel_Link>>?> GetAllDataByStatusAsync(bool filterByStatus)
+        {
+            var response = await _httpClient.GetAsync($"{url}/filterByStatus/{filterByStatus}");
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<Response<List<RequestViewModel_Link>>>(content, options: _options);
+            return result;
+        }
+
+        public async Task<Response<RequestViewModel_Link>?> GetDataByIdAsync(int? id)
+        {
+            var result = await _httpClient.GetFromJsonAsync<Response<RequestViewModel_Link>>($"{url}/filterById/{id}", options: _options);
+            return result;
+        }
+
+        public async Task<HttpResponseMessage> AddDataAsync(RequestViewModel_Link oLink)
+        {
+            //var json = JsonSerializer.Serialize(oCarrera);
+            //var content = new StringContent(json, Encoding.UTF8, "application/json");
+            //var response = await _httpClient.PostAsync(url, content);
+            var response = await _httpClient.PostAsJsonAsync(url, oLink, options: _options);
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> EditDataAsync(RequestViewModel_Link oLink)
+        {
+            //var json = JsonSerializer.Serialize(oCarrera);
+            //var content = new StringContent(json, Encoding.UTF8, "application/json");
+            //var response = await _httpClient.PutAsync(url, content);
+            var response = await _httpClient.PutAsJsonAsync(url, oLink, options: _options);
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> EnableDisableDataByIdAsync(int id, bool isActivate)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"{url}/editByIdStatus/{id}/{isActivate}",
+                new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+            return response;
+        }
+        
+    }
+}
