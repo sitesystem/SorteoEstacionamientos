94cf2acbb116e094d28430d60936fde8b2f82709
EduardoCarrasco3 <ecarrascocastillo3@gmail.com>
2024-05-06 13:59:17 -0600

    Update RepositorioController

--- SorteoEstacionamientos/Server/CapaDataAccess/Controllers/RepositorioFiles/RepositorioFilesController.cs
+++ SorteoEstacionamientos/Server/CapaDataAccess/Controllers/RepositorioFiles/RepositorioFilesController.cs
@@ -6,20 +6,77 @@ using System;
 using System.Net.Http.Headers;
 
 using SorteoEstacionamientos.Shared.CapaEntities.Response;
+using Microsoft.AspNetCore.Http.HttpResults;
 
-//namespace SorteoEstacionamientos.Server.CapaDataAccess.Controllers.RepositorioFiles
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class RepositorioFilesController(IHostEnvironment hostEnvironment, IWebHostEnvironment webHostEnvironment) : ControllerBase
-//    {
-//        private readonly IHostEnvironment _hostEnvironment = hostEnvironment;
-//        private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment;
+namespace SorteoEstacionamientos.Server.CapaDataAccess.Controllers.RepositorioFiles
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepositorioFilesController(IHostEnvironment hostEnvironment, IWebHostEnvironment webHostEnvironment) : ControllerBase
+    {
+        private readonly IHostEnvironment _hostEnvironment = hostEnvironment;
+        private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment;
 
-//        [HttpPost("[action]/{folder}/{id}/{filename}/{guid}")]
-//        public async Task<IActionResult> UploadSingFile(string folder, int id, string fileName, Guid guid, IFormFile file)
-//        {
+        [HttpPost("[action]/{folder}/{id}/{filename}/{guid}")]
+        public async Task<IActionResult> UploadSingFile(string folder, int id, string fileName, Guid guid, IFormFile file) //Guid = tokken
+        {
+            Response<object> oResponse = new();
+            try
+            {
+                if (file.Length > 0 && file != null)
+                {
+                    folder = Path.Combine(_webHostEnvironment.ContentRootPath, $"wwwroot/repositorio/{folder}/{id}");
+                    if (!Directory.Exists(folder))
+                        Directory.CreateDirectory(folder);
 
-//        }
-//    }
-//}
+                    fileName = $"{id}_{Path.GetFileNameWithoutExtension(fileName)}_{guid}{Path.GetExtension(file.FileName).ToLower()}";
+
+                    string dirPath = Path.Combine(folder, fileName);
+
+                    if (!System.IO.File.Exists(dirPath))
+                    {
+                        using FileStream oFileStream = new(dirPath, FileMode.Create, FileAccess.Write);
+                        await file.OpenReadStream().CopyToAsync(oFileStream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Clear();
+                Response.StatusCode = 200;
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "File removed successfully";
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = ex.Message;
+                oResponse.Message = ex.Message; 
+            }
+
+            return Ok(oResponse);
+        }
+    }
+
+    //[HttpPost("[action]/{folder}/{id}/{fileName}/{guid}")]
+    //public async Task<IActionResult> UploadMultipleFiles(string folder, int id, string fileName, Guid guid, IList<IFormFile> files)
+    //{
+    //    Response<object> oResponse = new();
+    //    try
+    //    {
+    //        if (files.Count < 1 || files.IsNullOrEmpty())
+    //            return BadRequest(oResponse);
+
+    //        foreach (var file in files)
+    //        {
+    //            folder = Path.Combine(_webHostEnviroment.ContentRootPath, $"wwwroot/repositorio/{folder}/{id}");
+
+    //            if(!Directory.Exists(folder))
+    //                Directory.
+                
+    //            fileName = $"{id}_{fileName}_{guid}{Path.GetExtension(file.FileName).ToLower()}";
+    //            long size = file.Length;
+    //        }
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        Response.Clear();
+    //    }
+    //    return Ok(oResponse);
+    //}
+}
