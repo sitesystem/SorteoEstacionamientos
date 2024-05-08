766e0666c408a9c3bbba1e09d19be0aea44365f1
EduardoCarrasco3 <ecarrascocastillo3@gmail.com>
2024-05-08 13:58:40 -0600

    Create Archivo para el servicio de archivos

---
+++ SorteoEstacionamientos/Shared/CapaServices_BusinessLogic/RArchivosService.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace SorteoEstacionamientos.Shared.CapaServices_BusinessLogic
+{
+    public class RArchivosService(HttpClient httpClient)
+    {
+
+    }
+}
