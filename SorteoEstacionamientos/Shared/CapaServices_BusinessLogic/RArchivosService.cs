4ce1764f38f7c3d0a50bbe1fba69bf283218acd4
EduardoCarrasco3 <ecarrascocastillo3@gmail.com>
2024-05-13 12:10:25 -0600

    Update Subida de Archivos

--- SorteoEstacionamientos/Shared/CapaServices_BusinessLogic/RArchivosService.cs
+++ SorteoEstacionamientos/Shared/CapaServices_BusinessLogic/RArchivosService.cs
@@ -1,5 +1,6 @@
-﻿
-using Microsoft.AspNetCore.Components.Forms;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using SorteoEstacionamientos.Shared.CapaEntities.Response;
+using SorteoEstacionamientos.Shared.Constantes;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -16,6 +17,48 @@ namespace SorteoEstacionamientos.Shared.CapaServices_BusinessLogic
 {
     public class RArchivosService(HttpClient httpClient)
     {
+        private readonly HttpClient _httpClient = httpClient;
+        private readonly JsonSerializerOptions _options = new() { PropertyNameCaseInsensitive =  true};
+        const string url = "/api/archivos";
 
+        public async Task<string?> SubirDocumento(TipoDocumento documento, IBrowserFile file)
+        {
+            string? result = null;
+            using (MultipartFormDataContent content = new MultipartFormDataContent())
+            {
+                using (StreamContent fileContent = new StreamContent(file.OpenReadStream()))
+                {
+                    fileContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+
+                    content.Add(fileContent, name: "\"file\"", file.Name);
+
+                    //SUBIR
+                    HttpResponseMessage httpResponse = await _httpClient.PostAsync($"{url}/subir/{(int)documento}", content);
+
+                    string json = await httpResponse.Content.ReadAsStringAsync();
+                    var response = JsonSerializer.Deserialize<Response<string?>>(json, options: _options);
+
+                    if (response is not null)
+                    {
+                        result = response.Data;
+                    }
+                }       
+            }
+            return result;
+        }
+
+        public async Task<Response<string>?> NewFile(string extension, string action)
+        {
+            var response = await _httpClient.GetAsync($"{url}/{extension}/{action}");
+
+            var content = await response.Content.ReadAsStringAsync() ;
+            var result = JsonSerializer.Deserialize<Response<string>>(content, options: _options);
+            return result;
+        }
+
+        //public async Task<Response<List<WebUtils.Link>>?> NewFileFromSelection<T>(string extension, string action, List<T> selected)
+        //{
+
+        //}
     }
 }
---
+++ SorteoEstacionamientos/Shared/Constantes/TipoDocumento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SorteoEstacionamientos.Shared.Constantes
+{
+    public enum TipoDocumento
+    {
+        CREDENCIAL = 0,
+        TARJETACIRCULACION = 1,
+        LICENCIACONDUCIR = 2,
+        COMPROBANTEESTUDIOS = 3,
+    }
+}
